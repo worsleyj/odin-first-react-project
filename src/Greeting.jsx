09c1751efcbba5c4a8e1ca516cefcdd7263b3c40
worsleyj <josephworsley23@gmail.com>
2024-09-12 14:53:44 -0400

    add animals list

---
+++ src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Introduction() {
+  return <h1>My name is Joey</h1>;
+}
+
+export default Introduction;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,51 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Introduction from "./Greeting.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Introduction />
     <App />
-  </StrictMode>,
-)
+    <Animals />
+  </StrictMode>
+);
+
+function Button(props) {
+  const buttonStyle = {
+    color: props.color,
+    fontSize: props.fontSize + "px",
+  };
+
+  return <button style={buttonStyle}>{props.text}</button>;
+}
+
+function List(props) {
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.animals.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
+
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <li key={animal}>{animal}</li>;
+      })}
+    </ul>
+  );
+}
+
+function Animals() {
+  const animals = ["Dog", "Cat"];
+
+  return (
+    <div>
+      <h1>Animals: </h1>
+      <List animals={animals} />
+    </div>
+  );
+}
