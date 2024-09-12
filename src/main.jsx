8662bf066f455c0fc6924d2a86604fc4bb735cf2
worsleyj <josephworsley23@gmail.com>
2024-09-12 14:59:44 -0400

    buttons

--- src/main.jsx
+++ src/main.jsx
@@ -9,16 +9,20 @@ createRoot(document.getElementById("root")).render(
     <Introduction />
     <App />
     <Animals />
+    <Button text="Click Me!" color="blue" fontSize={12} />
+    <Button text="Don't Click Me!" color="red" fontSize={12} />
+    <Button text="Click Me!" color="blue" fontSize={20} />
+    <Button />
   </StrictMode>
 );
 
-function Button(props) {
+function Button({ text = "Cewaelick Me!", color = "blue", fontSize = 12 }) {
   const buttonStyle = {
-    color: props.color,
-    fontSize: props.fontSize + "px",
+    color: color,
+    fontSize: fontSize + "px",
   };
 
-  return <button style={buttonStyle}>{props.text}</button>;
+  return <button style={buttonStyle}>{text}</button>;
 }
 
 function List(props) {
