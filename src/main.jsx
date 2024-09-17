4871c34a272ed42dcaa871db562c01b71b7b3460
worsleyj <josephworsley23@gmail.com>
2024-09-17 13:52:52 -0400

    add name

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import Name from "./Name.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Name />
   </StrictMode>
 );
