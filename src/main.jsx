b21bae4bac719dd3c81b7ff5a829a1d13b9c1256
worsleyj <josephworsley23@gmail.com>
2024-09-25 14:11:10 -0400

    add useEffect clock

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Form from "./components/Form.jsx";
+import Clock from "./components/Clock.jsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Form />
+    <Clock />
   </StrictMode>
 );
