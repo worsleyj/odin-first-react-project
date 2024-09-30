40b354b23abc62dad0227f9e94ec6d4bd2a199cb
worsleyj <josephworsley23@gmail.com>
2024-09-30 15:44:42 -0400

    finalize project

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -4,7 +4,6 @@ export default function Clock() {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    console.log("TEST");
     const key = setInterval(() => {
       setCounter((count) => count + 1);
     }, 1000);
